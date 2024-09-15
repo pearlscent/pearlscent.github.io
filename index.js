f2e671b673bddb9d8deaae3c890743176550e667
Harish Sharma <harisharma9332@gmail.com>
2024-09-15 19:01:21 +0530

    All product Pages and Redirection to Proper Product added

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 function redirect_product_page(id)
 {
     if(id=="FCJ-215006"){
-    location.href = "/products/FCJ-215006.html";
+        location.href = "/products/FCJ-215006.html";
     }
+    if(id=="FPJ-215006"){
+        location.href = "/products/FPJ-215006.html";
+    }
+    if(id=="FPJ-235006"){
+        location.href = "/products/FPJ-235006.html";
+    }
+    if(id=="FPJ-228006"){
+        location.href = "/products/FPJ-228006.html";
+    }
+    if(id=="FPJ-42005"){
+        location.href = "/products/FPJ-42005.html";
+    }
+    
+    
 }
